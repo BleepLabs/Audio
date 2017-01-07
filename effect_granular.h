8489b789b3610e4e42d0e192ca2ccc4b3ba57df9
Dr. Bleep <drbleep@bleeplabs.com>
2017-01-07 12:12:57 -0600

    Add files via upload

---
+++ effect_granular.h
@@ -0,0 +1,33 @@
+#include "AudioStream.h"
+
+class AudioEffectGranular : 
+public AudioStream
+{
+public:
+  AudioEffectGranular(void): 
+  AudioStream(1,inputQueueArray) { 
+  }
+
+  void begin(int16_t *sample_bank_def,int16_t max_len_def);
+  void length(int16_t max_len_def);
+  void rate(int16_t playpack_rate_def);
+
+  void freeze(int16_t activate,int16_t playpack_rate_def,int16_t grain_length_def);
+  void shift(int16_t activate,int16_t playpack_rate_def,int16_t grain_length_def);
+
+
+  virtual void update(void);
+  
+private:
+  audio_block_t *inputQueueArray[1];
+  int16_t *sample_bank;
+  int16_t max_sample_len;
+  int16_t write_head,read_head,grain_mode,freeze_len,allow_len_change;
+  int16_t playback_rate;
+  int16_t capture_trigger,capture_index,current_mode,playpack_rate;
+  int32_t accumulator,play_index,increment;
+  int16_t sample_loaded,prev_input,zero_cross_up,zero_cross_down,sample_trigger,write_en,glitch_cross_len,load_req,glitch_len,glitch_min_len,sample_req;
+
+
+};
+
---
+++ effect_sh.h
@@ -0,0 +1,60 @@
+#ifndef effect_sh_h_
+#define effect_sh_h_
+#include "AudioStream.h"
+#include "utility/dspinst.h"
+
+class AudioEffectSH : public AudioStream
+{
+public:
+  AudioEffectSH() : AudioStream(2, inputQueueArray) { }
+
+
+  virtual void update(void);
+  void begin(float rr_amt,short sm){
+    smooth(sm);
+    amount(rr_amt);
+  }
+
+  void smooth(short sm) {
+    smooth_en=sm;
+  }
+
+  void manual_hold(short mh) {
+    manual_hold_en=mh;
+  }
+
+  void amount(float rr_amt) {
+
+
+
+      if (rr_amt>0){
+        float a3t = pow(rr_amt, 2);
+        rate_reduction_amt =rr_amt* (65536.0f);
+        //rate_reduction_amt =rr_amt* (65536.0f);
+
+      }
+      if (rr_amt<=0){
+        rate_reduction_amt=0;
+      } 
+    
+
+    }     
+
+  
+
+
+ 
+
+
+private:
+  audio_block_t *inputQueueArray[2];
+  uint32_t rate_reduction_amt;  
+  int32_t  mix;  
+  int32_t  mix_inv;  
+  short smooth_en ;  
+  short manual_hold_en;  
+
+
+};
+
+#endif
